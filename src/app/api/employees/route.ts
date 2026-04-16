f33dff1e4d15a09e643b26e4becef689e2fdcfcc
bhandekunal16 <bhandekunal16@gmail.com>
2026-04-16 13:23:30 +0530

    refactor: improve code readability by simplifying query formatting and removing unnecessary line breaks

--- src/app/api/employees/route.ts
+++ src/app/api/employees/route.ts
@@ -3,15 +3,12 @@ import { clickhouse } from "@/app/lib/clickhouse";
 import { randomUUID } from "crypto";
 import config from "../../config/query.config.json";
 
-const { format }: any = config;
+const { format }: { format: any } = config;
 
 export async function GET() {
   try {
     const result = await clickhouse.query({
-      query: `SELECT * FROM employees
-              WHERE deleted = 0
-              ORDER BY created_at DESC
-              LIMIT 1 BY id`,
+      query: `SELECT * FROM employees WHERE deleted = 0 ORDER BY created_at DESC LIMIT 1 BY id`,
       format,
     });
     return NextResponse.json(await result.json());
@@ -50,13 +47,15 @@ export async function POST(req: NextRequest) {
 export async function PUT(req: NextRequest) {
   try {
     const body = await req.json();
-    
+
     if (!body.id) {
       return NextResponse.json({ error: "ID required" }, { status: 400 });
     }
 
+    const { name, email, salary, id } = body;
+
     const result = await clickhouse.query({
-      query: `ALTER TABLE employees update name = '${body.name}', email = '${body.email}', salary = '${body.salary}' where id = '${body.id}'`,
+      query: `ALTER TABLE employees UPDATE name = '${name}', email = '${email}', salary = '${salary}' WHERE id = '${id}'`,
       format,
     });
 
@@ -77,10 +76,7 @@ export async function DELETE(req: NextRequest) {
     }
 
     await clickhouse.command({
-      query: `
-        ALTER TABLE employees
-        DELETE WHERE id = '${body.id}'
-      `,
+      query: `ALTER TABLE employees DELETE WHERE id = '${body.id}'`,
     });
 
     return NextResponse.json({ success: true });
